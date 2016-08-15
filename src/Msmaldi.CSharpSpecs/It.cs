5e22b707cf1de242397807ada20a03c8d65b435d
Matheus Soares Maldi <msmaldi@hotmail.com>
2016-08-15 18:21:19 -0300

    Update project

--- src/Msmaldi.CSharpSpecs/It.cs
+++ src/Msmaldi.CSharpSpecs/It.cs
@@ -11,22 +11,8 @@ namespace Msmaldi.CSharpSpecs
 		internal override void Invoke(Action action, string text)
 		{
 			text = text.PadLeft(_ident + text.Length, ' ');
-
-			try
-			{
-				action.Invoke();
-				Console.ForegroundColor = ConsoleColor.Green;
-				Console.WriteLine($"{text}\tSuccedded");
-				Console.ResetColor();
-			}
-			catch (Exception ex)
-			{
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine($"{text}\tFailed");
-				Console.ResetColor();
-				System.Console.WriteLine("Exception");
-				throw ex;
-			}
+			action.Invoke();
+			Console.WriteLine(text);
 		}
 	}	
 }
--- test/Msmaldi.CSharpSpecs.Test/SpecContextTest.cs
+++ test/Msmaldi.CSharpSpecs.Test/SpecContextTest.cs
@@ -2,50 +2,50 @@ using Xunit;
 
 namespace Msmaldi.CSharpSpecs.Test
 {
-  // see example explanation on xUnit.net website:
-  // https://xunit.github.io/docs/getting-started-dotnet-core.html
-  public class SpecContextTest : SpecContext
-  {
-    [Fact]
-    public void PassingTest()
-    {
-      describe["when use SpecContext class"] = () =>
-      {
-        context["with other internal context"] = () =>
-        {
-          it["it is work very well"] = () => Assert.True(true);
-          it["it is work very well"] = () => Assert.True(true);
-          it["it is work very well"] = () => Assert.True(true);
-        };
+	// see example explanation on xUnit.net website:
+	// https://xunit.github.io/docs/getting-started-dotnet-core.html
+	public class SpecContextTest : SpecContext
+	{
+		[Fact]
+		public void PassingTest()
+		{
+			describe["when use SpecContext class"] = () =>
+			{
+				context["with other internal context"] = () =>
+				{
+					it["it is work very well"] = () => Assert.True(true);
+					it["it is work very well"] = () => Assert.True(true);
+					it["it is work very well"] = () => Assert.True(true);
+				};
 
-        context["with second internal context"] = () =>
-        {
-          it["it is work very well too!!!"] = () => Assert.True(true);
-        };
+				context["with second internal context"] = () =>
+				{
+					it["it is work very well too!!!"] = () => Assert.True(true);
+				};
 
-        context["Great!!!"] = () => Assert.True(true);
-      };
-    }
+				context["Great!!!"] = () => Assert.True(true);
+			};
+		}
 
-    //[Fact]
-    public void FailingTest()
-    {
-      describe["when use SpecContext class for failing"] = () =>
-      {
-        context["with other internal context for failing"] = () =>
-        {
-          it["it is not work very well"] = () => Assert.True(false);
-          it["it is work work very well"] = () => Assert.True(true);
-          it["it is not very well"] = () => Assert.True(false);
-        };
+		[Fact]
+		public void FailingTest()
+		{
+			describe["when use SpecContext class for failing"] = () =>
+			{
+				context["with other internal context for failing"] = () =>
+				{
+					it["it is not work very well"] = () => Assert.True(false);
+					it["it is work work very well"] = () => Assert.True(true);
+					it["it is not very well"] = () => Assert.True(false);
+				};
 
-        context["with second internal context"] = () =>
-        {
-          it["it is work very well too!!!"] = () => Assert.True(true);
-        };
+				context["with second internal context"] = () =>
+				{
+					it["it is work very well too!!!"] = () => Assert.True(true);
+				};
 
-        context["Great!!!"] = () => Assert.True(true);
-      };
-    }
-  }
+				context["Great!!!"] = () => Assert.True(true);
+			};
+		}
+	}
 }
